3e01ec7dc6718640cb9a6eab01c4145cb588efa5
satya sai <62896328+satyasai69@users.noreply.github.com>
2024-04-28 18:16:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1237,9 +1237,9 @@ export default function Home({ sendDataToParent }) {
                   </div>
                 </div>
                 <input
-                 // type="text"
-    type="number"
-  step="any
+                 type="text"
+   // type="number"
+  //step="any
                   placeholder="0.00000"
                   className="bg-transparent flex-1 outline-none border-none text-base text-[#ebefe9]"
                   value={value}
@@ -1290,9 +1290,9 @@ export default function Home({ sendDataToParent }) {
                   </div>
                 </div>
                 <input
-                  // type="text"
-    type="number"
-  step="any
+                   type="text"
+   // type="number"
+  //step="any
                   placeholder="0.00000"
                   className="bg-transparent flex-1 outline-none border-none text-base text-[#ebefe9]"
                   value={value}
